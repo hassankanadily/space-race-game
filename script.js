f3a8f7a948d9af67eee2b040b7bb0f6c8d70ce5a
hassankanadily <hassansk2007@gmail.com>
2026-05-21 00:12:24 +0300

    update

--- script.js
+++ script.js
@@ -56,119 +56,110 @@ questionBox.addEventListener('click',()=>{
       starsEl.appendChild(s);
   }*/
 
-
-const questionBox = document.querySelector('.question-box');
+const questionBox = document.querySelector(".question-box");
 const answers = document.querySelectorAll(".actual-answers");
 
-let sectionList = document.querySelectorAll('section');
-function showPage(id){
-  sectionList.forEach(section => {
+let sectionList = document.querySelectorAll("section");
+function showPage(id) {
+  sectionList.forEach((section) => {
     section.hidden = true;
   });
   document.getElementById(id).hidden = false;
-  window.scrollTo(0,0);
+  window.scrollTo(0, 0);
 }
-showPage('select-level-sec');
+showPage("select-level-sec");
 
-const gradeLevels = document.querySelectorAll('.grade-btn');
-gradeLevels.forEach(grade =>{
-  grade.addEventListener('click',()=>{
-    showPage('select-subject-sec');
+const gradeLevels = document.querySelectorAll(".grade-btn");
+gradeLevels.forEach((grade) => {
+  grade.addEventListener("click", () => {
+    showPage("select-subject-sec");
   });
 });
 
-const subjectsSelection = document.querySelectorAll('.new-subject');
-subjectsSelection.forEach(subject =>{
-  subject.addEventListener('click',()=>{
-    showPage('start-quiz-sec');
+const subjectsSelection = document.querySelectorAll(".new-subject");
+subjectsSelection.forEach((subject) => {
+  subject.addEventListener("click", () => {
+    showPage("start-quiz-sec");
   });
 });
-
-
-
-
-
 
 let startQuizBtn = document.getElementById("startQuiz");
 
-startQuizBtn.addEventListener('click',()=>{
-  showPage('quiz');
+startQuizBtn.addEventListener("click", () => {
+  showPage("quiz");
 });
 
 const playAgainBtn = document.querySelector(".play-again-button");
 
-playAgainBtn.addEventListener('click',()=>{
+playAgainBtn.addEventListener("click", () => {
   quizSection.style.opacity = "1";
   winnerSection.style.transform = "translateY(100%)";
   quizSection.style.transform = "translateY(0)";
 
-  setTimeout(()=>{
+  setTimeout(() => {
     winnerSection.style.opacity = "0";
     winnerSection.style.transform = "translateY(-100%)";
     //winnerSection.style.display = "none";
-  },1000);
+  }, 1000);
 });
 
-
-const levels = document.querySelectorAll('.new-level');
+const levels = document.querySelectorAll(".new-level");
 let selectedLevel = null;
-levels.forEach(level =>{
-  level.addEventListener('click',()=>{
+levels.forEach((level) => {
+  level.addEventListener("click", () => {
     selectedLevel = level.value;
     console.log(level.value);
     tryFetchQuestions();
   });
 });
 
-const subjects = document.querySelectorAll('.new-subject');
+const subjects = document.querySelectorAll(".new-subject");
 let selectedSubject = null;
-subjectsSelection.forEach(subject => {
-  subject.addEventListener('click', ()=>{
-    selectedSubject = subject.getAttribute('value');
-    console.log(subject.getAttribute('value'));
+subjectsSelection.forEach((subject) => {
+  subject.addEventListener("click", () => {
+    selectedSubject = subject.getAttribute("value");
+    console.log(subject.getAttribute("value"));
     tryFetchQuestions();
   });
 });
 
-
 let questionsSent = [];
-function tryFetchQuestions(){
-  if(selectedLevel && selectedSubject){
+function tryFetchQuestions() {
+  if (selectedLevel && selectedSubject) {
     console.log("hi");
     fetch(`/questions/${selectedLevel}/${selectedSubject}`)
-    .then(res => res.json())
-    .then(data =>{
-      questionsSent = data;
-      console.log("Questions received:", data);
-      selectedLevel = null;
-      selectedSubject = null;
-    });
+      .then((res) => res.json())
+      .then((data) => {
+        questionsSent = data;
+        console.log("Questions received:", data);
+        selectedLevel = null;
+        selectedSubject = null;
+      });
   }
 }
 
 let currentIndex = 0;
-function displayQuestions(data){
+function displayQuestions(data) {
   console.log("eshtaghal");
   let currentQuestion = data[currentIndex]["question"];
-      let A = data[currentIndex]["options"]["A"];
-      let B = data[currentIndex]["options"]["B"];
-      let C = data[currentIndex]["options"]["C"];
-      let D = data[currentIndex]["options"]["D"];
+  let A = data[currentIndex]["options"]["A"];
+  let B = data[currentIndex]["options"]["B"];
+  let C = data[currentIndex]["options"]["C"];
+  let D = data[currentIndex]["options"]["D"];
 
-      document.getElementById("displayed-question").textContent = currentQuestion;
-      document.getElementById("a").textContent = A;
-      document.getElementById("b").textContent = B;
-      document.getElementById("c").textContent = C;
-      document.getElementById("d").textContent = D;
-      if(currentIndex < 7){
-        currentIndex++;
-      }
-      else{
-        console.log("Questions Completed");
-      }
+  document.getElementById("displayed-question").textContent = currentQuestion;
+  document.getElementById("a").textContent = A;
+  document.getElementById("b").textContent = B;
+  document.getElementById("c").textContent = C;
+  document.getElementById("d").textContent = D;
+  if (currentIndex < 7) {
+    currentIndex++;
+  } else {
+    console.log("Questions Completed");
+  }
 }
 
-startQuizBtn.addEventListener('click',()=>{
+startQuizBtn.addEventListener("click", () => {
   displayQuestions(questionsSent);
 });
 
@@ -212,12 +203,12 @@ let counter = 0;
 
 const socket = io();
 
-socket.on('answer-detected', (data) => {
-  console.log('received');
+socket.on("answer-detected", (data) => {
+  console.log("received");
   console.log(data);
 
-  if(letter === null){
-    if(counter === currentIndex - 1 && data.sensor !== null){
+  if (letter === null) {
+    if (counter === currentIndex - 1 && data.sensor !== null) {
       checkAnswer(data);
     }
   }
@@ -225,86 +216,149 @@ socket.on('answer-detected', (data) => {
 
 let car1Score = 0;
 let car2Score = 0;
+let progressWidth = 0;
 
-function checkAnswer(data){
-  console.log('hi');
-  if(data.sensor === null){
+function checkAnswer(data) {
+  console.log("hi");
+  if (data.sensor === null) {
     return;
   }
-  if(letter === null){
+  if (letter === null) {
     letter = data.sensor;
     carId = data.uid;
-    let answer = questionsSent[currentIndex - 1]['answer'];
+    let answer = questionsSent[currentIndex - 1]["answer"];
     console.log(answer);
     console.log(letter);
     console.log(currentIndex);
-    if(letter === answer){
+    if (letter === answer) {
       counter++;
-      if(carId === '03C5EF2C'){
+      if (carId === "03C5EF2C") {
         car1Score++;
-        document.getElementById('person1-points').textContent = car1Score;
-        console.log('Car 1 Score: ',car1Score);
-        if(counter === 7){
-          showPage('winner');
+        document.getElementById("person1-points").textContent = car1Score;
+        console.log("Car 1 Score: ", car1Score);
+        if (counter === 7) {
+          checkWinner(car1Score, car2Score);
+          showPage("winner");
+        }
+      } else {
+        car2Score++;
+        document.getElementById("person2-points").textContent = car2Score;
+        console.log("Car 2 Score: ", car2Score);
+        if (counter === 7) {
+          checkWinner(car1Score, car2Score);
+          showPage("winner");
         }
       }
-      else{
-        car2Score++;
-        document.getElementById('person2-points').textContent = car2Score;
-        console.log('Car 2 Score: ',car2Score);
-      }
-      if(currentIndex<=6){
-        fetch('/reset-answer', {
-          method: 'POST'
+      if (currentIndex <= 6) {
+        fetch("/reset-answer", {
+          method: "POST",
         })
-        .then(res =>res.json())
-        .then(data=>{
-          data.sensor = null;
-          data.uid = null;
-        })
+          .then((res) => res.json())
+          .then((data) => {
+            data.sensor = null;
+            data.uid = null;
+          });
 
         console.log("Correct Answer");
         correctAnswer(letter);
+        progressWidth += 15;
+        progress.style.width = progressWidth.toString() + "%";
         letter = null;
         carId = null;
       }
-    }
-    else{
-      console.log('Incorrect Answer');
+    } else {
+      console.log("Incorrect Answer");
       wrongAnswer(letter);
       letter = null;
       carId = null;
-      fetch('/reset-answer', {
-          method: 'POST'
+      fetch("/reset-answer", {
+        method: "POST",
       })
-      .then(res =>res.json())
-      .then(data=>{
-        data.sensor = null;
-        data.uid = null;
-      })
-    } 
+        .then((res) => res.json())
+        .then((data) => {
+          data.sensor = null;
+          data.uid = null;
+        });
+    }
   }
 }
 
-function correctAnswer(answer){
+function correctAnswer(answer) {
   const lowerAnswer = answer.toLowerCase();
   const currentAnswer = document.getElementById(lowerAnswer);
   console.log(currentAnswer.className);
   console.log(lowerAnswer);
-  currentAnswer.classList.add('correct');
-  setTimeout(()=>{
-    currentAnswer.classList.remove('correct');
+  currentAnswer.classList.add("correct");
+  setTimeout(() => {
+    currentAnswer.classList.remove("correct");
     displayQuestions(questionsSent);
-  },2000);
+  }, 2000);
 }
 
-function wrongAnswer(answer){
+function wrongAnswer(answer) {
   const lowerAnswer = answer.toLowerCase();
   const currentAnswer = document.getElementById(lowerAnswer);
   console.log(currentAnswer.className);
-  console.log(lowerAnswer);2
-  currentAnswer.classList.add('wrong');
-  setTimeout(()=>{
-    currentAnswer.classList.remove('wrong');
-  },2000);
+  console.log(lowerAnswer);
+  2;
+  currentAnswer.classList.add("wrong");
+  setTimeout(() => {
+    currentAnswer.classList.remove("wrong");
+  }, 2000);
+}
+
+const char1Buttons = document.querySelectorAll(".char1-button");
+const char1Character = document.querySelector(".person1-img p");
+char1Buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    char1Character.textContent = button.textContent;
+  });
+});
+const char2Buttons = document.querySelectorAll(".char2-button");
+const char2Character = document.querySelector(".person2-img p");
+char2Buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    char2Character.textContent = button.textContent;
+  });
+});
+
+const person1Value = document.getElementById("player-1-name");
+const person2Value = document.getElementById("player-2-name");
+
+const name1 = document.querySelector(".person1-name");
+const name2 = document.querySelector(".person2-name");
+
+startQuizBtn.addEventListener("click", () => {
+  const person1Name = person1Value.value;
+  const person2Name = person2Value.value;
+
+  name1.textContent = person1Name;
+  name2.textContent = person2Name;
+});
+
+const progress = document.querySelector(".progress-filler");
+const winnerName = document.querySelector(".winner-score-name");
+const winnerPoints = document.querySelector(".winner-points");
+const secondName = document.querySelector(".second-score-name");
+const secondPoints = document.querySelector(".second-points");
+
+function checkWinner(player1Points, player2Points) {
+  if (player1Points > player2Points) {
+    /*const points1 = parseInt(
+      document.getElementById("person1-points").textContent,
+    );
+    const points2 = parseInt(
+      document.getElementById("person1-points").textContent,
+    );*/
+
+    winnerName.textContent = name1.textContent;
+    winnerPoints.textContent = player1Points;
+    secondName.textContent = name2.textContent;
+    secondPoints.textContent = player2Points;
+  } else {
+    winnerName.textContent = name2.textContent;
+    winnerPoints.textContent = player2Points;
+    secondName.textContent = name1.textContent;
+    secondPoints.textContent = player1Points;
+  }
 }
